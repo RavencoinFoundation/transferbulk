6c3a471e82e98906414c5730adbced333cd8dec9
Tron <tron@Trons-MacBook-Pro.local>
2021-03-22 21:16:09 -0600

    Check for dups

--- transferbulk.py
+++ transferbulk.py
@@ -87,11 +87,11 @@ def get_rpc_connection():
     return(rpc_connection)
 
 def is_sent(trans, csv_outfile):
-    csvout = csv.DictReader(csv_outfile)
-    if csvout:
-        for sent in csvout:
-            if (sent['asset'] == trans['asset'] and sent['qty'] == trans['qty'] and sent['address'] == trans['address'] and sent['ipfs'] == trans['ipfs']):
-                return(True)
+    with open(csv_outfile, newline='') as csvout:
+    csvout = csv.DictReader(csvout)
+    for sent in csvout:
+        if (sent['asset'] == trans['asset'] and sent['qty'] == trans['qty'] and sent['address'] == trans['address'] and sent['ipfs'] == trans['ipfs']):
+            return(True)
     return(False)
 
 # def add_to_ipfs(file):
